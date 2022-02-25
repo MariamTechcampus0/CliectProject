7e482ba4a12bd39098697cdc13e3194006af4afa
MariamTechcampus0 <ma.maliki.ma@gmail.com>
2022-02-25 15:46:49 +0300

    New Update

---
+++ CliectProject/Content/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CliectProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
