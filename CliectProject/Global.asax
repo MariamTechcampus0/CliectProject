e86d276374fc18ca2b77ec383ea5141245cc581b
MariamTechcampus0 <ma.maliki.ma@gmail.com>
2022-02-25 08:49:24 +0300

    Add project files.

---
+++ CliectProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CliectProject.MvcApplication" Language="C#" %>
---
+++ CliectProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CliectProject.Startup))]
+namespace CliectProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
